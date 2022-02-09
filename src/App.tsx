a1842a742d723aaf076e1466a019e7eaefcee2b4
samian016 <walidbinjashim@gmail.com>
2022-02-09 20:49:55 +0600

    added some things samian

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import { Link } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-// import required modules
-import { FreeMode, Pagination, Navigation } from "swiper";
+import DailyBest from './Components/DailyBest/DailyBest';
+import SupplyerBanner from './Components/SupplyerBanner/SupplyerBanner';
 function App() {
   return (
     <div>
-     
+      <SupplyerBanner></SupplyerBanner>
+      <DailyBest></DailyBest>
     </div>
   );
 }
 
-export default App;
+export default App;/* w-50 ms-auto mb-auto */
