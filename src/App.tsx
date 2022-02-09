d9ef286ec21809af73881bac466d6389e65eda3a
Emon Ahmed <technicalboyemon@gmail.com>
2022-02-09 21:05:55 +0600

    Conflict Solving - Emon

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-// import required modules
-import { FreeMode, Pagination, Navigation } from "swiper";
 import Category from './Components/Category/Category';
+
 function App() {
   return (
     <div>
