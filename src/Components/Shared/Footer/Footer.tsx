59ba241f1e29ba57b2534ba530a0d007c01f3b3c
nafish nobel <nobel15-11651@diu.edu.bd>
2022-02-16 00:24:44 +0600

    oo

--- src/Components/Shared/Footer/Footer.tsx
+++ src/Components/Shared/Footer/Footer.tsx
@@ -5,8 +5,7 @@ const Footer = () => {
     return (
         <div className='container mt-2'>
             <div className='bg-dark rounded p-1'>
-                <h1 className='text-white'>This is footer</h1>
-
+                <h1 className='text-white text-center'>This is footer</h1>
                 <div className="container my-2">
                     <div className="row g-2">
                         <div className="col-lg-3">
