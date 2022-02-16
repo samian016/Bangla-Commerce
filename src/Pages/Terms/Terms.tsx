ec950cc927a152a2b838c51848a26a43ca79ad66
nafish nobel <nobel15-11651@diu.edu.bd>
2022-02-17 00:19:46 +0600

    second push

--- src/Pages/Terms/Terms.tsx
+++ src/Pages/Terms/Terms.tsx
@@ -6,48 +6,23 @@ const Terms = () => {
             <>
                 <div className='container mx-2'>
 
-                    <h1>Privacy Policy</h1>
-                    <h3>Welcome to AliThemes’s Privacy Policy</h3>
-                    <p>1. Hi there, we’re AliThemes Pty Ltd (ABN 11 119 159 741) of Level 1,
-                        121 King Street Melbourne, 3000, Australia (“AliThemes”) and welcome
-                        to our privacy policy which also applies to our Affiliate Companies.
-                        This policy sets out how we handle your personal information if
-                        you’re an AliThemes user or visitor to our Sites. It applies across
-                        AliThemes Elements, AliThemes Market, AliThemes Twenty20, AliThemes Studio,
-                        AliThemes Sites, AliThemes Tuts+ and Placeit (the “Sites”).
+                    <h1>Terms of Service</h1>
+                    <h3>By Jonh
+                        9 April 2020
+                        8 mins read
+                        29k Views</h3>
+                    <p>Please read these Terms of Service (“Terms”, “Terms of Service”) carefully before using the https://Bangla Ecommarce.com website (the “Service”) operated by Evara (“us”, “we”, or “our”).
 
                         <br />
                         <br />
-                        2. When we say ‘we’, ‘us’ or ‘AliThemes’ it’s because that’s who we are and we own and run the Sites.
+                        Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.
                         <br />
                         <br />
-                        3.If we say ‘policy’ we’re talking about this privacy policy. If we say ‘user terms’ we’re talking
-                        about the rules for using each of the Sites. The rules vary by product and each product makes them
-                        separately available and seeks consent to them separately to this policy.
+                        By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.
                     </p>
-
-                    <h3>The type of personal information we collect</h3>
-                    <h3>How we collect personal information</h3>
-                    <h3>Personal information we collect about you from others</h3>
-                    <h3>How we use personal information</h3>
-                    <h3>When we disclose your personal information</h3>
-                    <h3>Where we transfer and/or store your personal information</h3>
-                    <h3>How we keep your personal information secure</h3>
-                    <h3>How you can access your personal information</h3>
-                    <h3>Marketing Choices regarding your personal information</h3>
-                    <h3>Cookies (not the type you eat!) and web analytics</h3>
-                    <h3>Information about children</h3>
-                    <h3>Information you make public or give to others</h3>
-                    <h3>How long we keep your personal information</h3>
-                    <h3>When we need to update this policy</h3>
-                    <h3>How you can contact us</h3>
-                    <h3>If you’re a user or visitor in the European Economic Area these rights also apply to you:</h3>
-                    <h3>How you can access your personal information</h3>
 
                 </div>
             </>
         </div>
     );
 };
-
-export default Terms;
