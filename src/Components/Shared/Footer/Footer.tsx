893b97963b777150217d51ae8ef72b67dac7805a
nafish nobel <nobel15-11651@diu.edu.bd>
2022-02-17 00:11:08 +0600

    footer 2nd section

--- src/Components/Shared/Footer/Footer.tsx
+++ src/Components/Shared/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import logo1 from './images/logo.png';
 import logo2 from './images/app-store.jpg';
 import logo3 from './images/google-play.jpg';
 import logo4 from './images/payment-method.png';
-import "./Footer.css";
+import './Footer.css';
 
 const Footer = () => {
     return (
@@ -45,17 +45,17 @@ const Footer = () => {
                             <div className="p-3 ">
                                 <h6 className='font-h6 text-muted'>Company</h6>
                                 <h6 className='font-h6'>About Us</h6>
-                                <h6 className='font-h6'>Delivery Information</h6>
-                                <h6 className='font-h6'>Privacy Policy</h6>
-                                <h6 className='font-h6'>Contact Us</h6>
-                                <h6 className='font-h6'> Support Center</h6>
-                                <h6 className='font-h6'>Careers</h6>
+                                <h6 className='font-h6 mt-4'>Delivery Information</h6>
+                                <h6 className='font-h6 mt-3'>Privacy Policy</h6>
+                                <h6 className='font-h6 mt-3'>Contact Us</h6>
+                                <h6 className='font-h6 mt-3'> Support Center</h6>
+                                <h6 className='font-h6 mt-3'>Careers</h6>
                             </div>
                         </div>
                         <div className="col-lg-2">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Account</h1>
-                                <h6 className='font-h6'>Sign In</h6>
+                                <h6 className='font-h6 mt-4'>Sign In</h6>
                                 <h6 className='font-h6'>View Cart</h6>
                                 <h6 className='font-h6'>My Wishlist</h6>
                                 <h6 className='font-h6'>Track My Order</h6>
@@ -68,7 +68,7 @@ const Footer = () => {
                         <div className="col-lg-2">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Corporate</h1>
-                                <h6 className='font-h6'>Become a Vendor</h6>
+                                <h6 className='font-h6 mt-4'>Become a Vendor</h6>
                                 <h6 className='font-h6'>Affiliate Program</h6>
                                 <h6 className='font-h6'>Farm Business</h6>
                                 <h6 className='font-h6'>Farm Careers</h6>
@@ -80,37 +80,74 @@ const Footer = () => {
                         <div className="col-lg-2">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Popular</h1>
-                                <h6 className='font-h6'>Milk & Flavoured Milk</h6>
+                                <h6 className='font-h6 mt-4'>Milk & Flavoured Milk</h6>
                                 <h6 className='font-h6'>Butter and Margarine</h6>
                                 <h6 className='font-h6'>Eggs Substitutes</h6>
                                 <h6 className='font-h6'>Marmalades</h6>
                                 <h6 className='font-h6'>Sour Cream and Dips</h6>
                                 <h6 className='font-h6'>Tea & Kombucha</h6>
                                 <h6 className='font-h6'>Cheese</h6>
-                              
+
                             </div>
                         </div>
                         <div className="col-lg-2">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Install Apps</h1>
                                 <h5 className='font-h6'>From Apps Store or Google Play</h5>
-                                <div>
+                                <div className='my-4'>
                                     <a href="">
-                                        <img src={logo2} alt="" className="img-fluid" />
+                                        <img src={logo2} alt="" className="w-50 p-1" />
                                     </a>
                                     <a href="">
-                                        <img src={logo3} alt="" className="img-fluid" />
+                                        <img src={logo3} alt="" className="w-50 p-1" />
                                     </a>
                                 </div>
                                 <div>
-                                    <h5 className='font-h6'>Secured Payment Gateways</h5>
+                                    <h5 className=''>Secured Payment Gateways</h5>
                                     <a href="">
-                                        <img src={logo4} alt="" className="img-fluid" />
+                                        <img src={logo4} alt="" className="img-fluid my-3" />
                                     </a>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    {/* 2nd footer */}
+
+
+                    <div className="hr"><hr /></div>
+
+                    <div className="mt-2">
+                        <div className="row g-2">
+                            <div className="col-lg-3">
+                                <div className="p-3">
+                                    <p className='text-secondary'>© 2021,<span className='text-success fw-bold'>Bangla</span>-Ecommerce</p>
+                                    <p  className='text-secondary'>All rights reserved</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3">
+                                <div className="p-3 ">
+                                    <h3 className='fw-bold text-success'>1900 - 6666</h3>
+                                    <p  className='text-secondary'>Working 8:00 - 22:00</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3">
+                                <div className="p-3 ">
+                                    <h3 className='fw-bold text-success'>1900 - 8888</h3>
+                                    <p  className='text-secondary'>24/7 Support Center</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3">
+                                <div className="p-3 ">
+                                    <h6>Follow Us</h6>
+                                    <p  className='text-secondary '>Up to 15% discount on your first subscribe</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
 
--- src/Pages/Terms/Terms.tsx
+++ src/Pages/Terms/Terms.tsx
@@ -2,8 +2,50 @@ import React from 'react';
 
 const Terms = () => {
     return (
-        <div>
-            <>Terms</>
+        <div className='container mt-2 overflow-hidden'>
+            <>
+                <div className='container mx-2'>
+
+                    <h1>Privacy Policy</h1>
+                    <h3>Welcome to AliThemes’s Privacy Policy</h3>
+                    <p>1. Hi there, we’re AliThemes Pty Ltd (ABN 11 119 159 741) of Level 1,
+                        121 King Street Melbourne, 3000, Australia (“AliThemes”) and welcome
+                        to our privacy policy which also applies to our Affiliate Companies.
+                        This policy sets out how we handle your personal information if
+                        you’re an AliThemes user or visitor to our Sites. It applies across
+                        AliThemes Elements, AliThemes Market, AliThemes Twenty20, AliThemes Studio,
+                        AliThemes Sites, AliThemes Tuts+ and Placeit (the “Sites”).
+
+                        <br />
+                        <br />
+                        2. When we say ‘we’, ‘us’ or ‘AliThemes’ it’s because that’s who we are and we own and run the Sites.
+                        <br />
+                        <br />
+                        3.If we say ‘policy’ we’re talking about this privacy policy. If we say ‘user terms’ we’re talking
+                        about the rules for using each of the Sites. The rules vary by product and each product makes them
+                        separately available and seeks consent to them separately to this policy.
+                    </p>
+
+                    <h3>The type of personal information we collect</h3>
+                    <h3>How we collect personal information</h3>
+                    <h3>Personal information we collect about you from others</h3>
+                    <h3>How we use personal information</h3>
+                    <h3>When we disclose your personal information</h3>
+                    <h3>Where we transfer and/or store your personal information</h3>
+                    <h3>How we keep your personal information secure</h3>
+                    <h3>How you can access your personal information</h3>
+                    <h3>Marketing Choices regarding your personal information</h3>
+                    <h3>Cookies (not the type you eat!) and web analytics</h3>
+                    <h3>Information about children</h3>
+                    <h3>Information you make public or give to others</h3>
+                    <h3>How long we keep your personal information</h3>
+                    <h3>When we need to update this policy</h3>
+                    <h3>How you can contact us</h3>
+                    <h3>If you’re a user or visitor in the European Economic Area these rights also apply to you:</h3>
+                    <h3>How you can access your personal information</h3>
+
+                </div>
+            </>
         </div>
     );
 };
