8d52b64f9c22d96f23545ec2d31e9f3ed80e6bbd
nafish nobel <nobel15-11651@diu.edu.bd>
2022-03-07 12:03:17 +0600

    sponsor's headings added

--- src/Components/Sponsor/Sponsor.tsx
+++ src/Components/Sponsor/Sponsor.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Sponsor = () => {
     return (
-        <div>
-            <h1>this is sponsor</h1>
+        <div className='my-2 mx-3'>
+            <h1 className='text-center fw-bold text-secondary'>Sponsor's and Partners</h1>
+
+            <h2 className='bg-success rounded text-center text-white fw-bold'>Platinum Sponsors</h2>
+            <h2 className='bg-success rounded text-center text-white fw-bold'>Gold Sponsors</h2>
+            <h2 className='bg-success rounded text-center text-white fw-bold'>silver Sponsors</h2>
+            <h2 className='bg-success rounded text-center text-white fw-bold'>Bonze Sponsors</h2>
+
         </div>
     );
 };
