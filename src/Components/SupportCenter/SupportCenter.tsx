620d7a5f217ee36d198c6a2d91d02911ca7778a6
nafish nobel <nobel15-11651@diu.edu.bd>
2022-03-18 05:45:42 +0600

    supportcenter

--- src/Components/SupportCenter/SupportCenter.tsx
+++ src/Components/SupportCenter/SupportCenter.tsx
@@ -45,13 +45,21 @@ const SupportCenter = () => {
                                     SupportData.map((sData) =>
 
                                         <Col>
-                                            <Card>
-                                                <Card.Img variant="top" src={sData.images} />
+                                            <Card className='my-1'>
+                                                <Card.Img variant="top" className='img-fluid'   src={sData.images} />
                                                 <Card.Body>
-                                                    <Card.Title>{sData.name}</Card.Title>
-                                                    <Card.Text>
-                                                        {sData.details}
+                                                    <Card.Title className='fw-bold text-center text-warning bg-light rounded'>{sData.name}</Card.Title>
+                                                    <Card.Text className=''>
+                                                       <span >
+                                                       {sData.details}
+                                                       </span> 
                                                     </Card.Text>
+
+                                                        <a className='my-1 text-center' href="http://nurnafishahmednobel@gmail.com">
+                                                        <button type="button" className="btn btn-info">Email</button>
+                                                        </a>
+                                                    
+                                                    
                                                 </Card.Body>
                                             </Card>
                                         </Col>
@@ -66,7 +74,7 @@ const SupportCenter = () => {
                     </div>
                     <div className="col-6">
                         <div className="p-3 border bg-light">
-                            
+                            <h1>second part</h1>
                         </div>
                     </div>
 
