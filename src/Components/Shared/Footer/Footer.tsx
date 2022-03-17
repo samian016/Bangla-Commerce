75e928fccc94dbda62a92f8977a61949f7dce98d
nafish nobel <nobel15-11651@diu.edu.bd>
2022-03-18 01:51:52 +0600

    footer solve

--- src/Components/Shared/Footer/Footer.tsx
+++ src/Components/Shared/Footer/Footer.tsx
@@ -3,12 +3,18 @@ import logo1 from './../../../Image/logo.png';
 import logo2 from './images/app-store.jpg';
 import logo3 from './images/google-play.jpg';
 import logo4 from './images/payment-method.png';
+import useAuth from '../../../Hooks/useAuth';
 import './Footer.css';
 import { Link } from 'react-router-dom';
 
-{/* <Link to=""></Link> */}
+{/* <Link to=""></Link> */ }
+
+
 
 const Footer = () => {
+
+    let { user } = useAuth();
+
     return (
         <div className='container-fluid overflow-hidden my-3'>
             <div className='mx-5'>
@@ -46,37 +52,39 @@ const Footer = () => {
                             <div className="p-3 ">
                                 <h1 className='font-h6'>Company</h1>
                                 <Link to="/about">
-                                <h6 className='font-h6'>About Us</h6>
+                                    <h6 className='font-h6'>About Us</h6>
                                 </Link>
-                                
+
                                 {/* <h6 className='font-h6'>Delivery Information</h6> */}
                                 <Link to="/terms">
-                                <h6 className='font-h6'>Privacy Policy</h6>
+                                    <h6 className='font-h6'>Privacy Policy</h6>
                                 </Link>
-                                
+
                                 <Link to="/contact">
-                                <h6 className='font-h6'>Contact Us</h6>
+                                    <h6 className='font-h6'>Contact Us</h6>
                                 </Link>
                                 <Link to="/supportCenter">
-                                <h6 className='font-h6'> Support Center</h6>
+                                    <h6 className='font-h6'> Support Center</h6>
                                 </Link>
-                                
+
                                 {/* <h6 className='font-h6'>Careers</h6> */}
                             </div>
                         </div>
                         <div className="col-lg-2">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Account</h1>
-                                <Link to="/login">
-                                <h6 className='font-h6'>Sign In</h6>
-                                </Link> 
+                                {
+                                    user ? <div>nnnn</div> : <Link to="/login">
+                                        <h6 className='font-h6'>Sign In</h6>
+                                    </Link>
+                                }
                                 <Link to="/checkout">
-                                <h6 className='font-h6'>View Cart</h6></Link>                               
-                                
+                                    <h6 className='font-h6'>View Cart</h6></Link>
+
                                 {/* <h6 className='font-h6'>My Wishlist</h6> */}
                                 {/* <h6 className='font-h6'>Track My Order</h6> */}
                                 <Link to="">
-                                <h6 className='font-h6'>Help Ticket</h6>
+                                    <h6 className='font-h6'>Help Ticket</h6>
                                 </Link>
                                 {/* <h6 className='font-h6'>Shipping Details</h6>
                                 <h6 className='font-h6'>Compare products</h6> */}
@@ -99,20 +107,24 @@ const Footer = () => {
                         <div className="col-lg-2 col-md-3">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Corporate</h1>
-                                <Link to="/singUp">
-                                <h6 className='font-h6 mt-4'>Become a Vendor</h6>
-                                </Link>
-                                
+
+                                {
+                                    user ? <div>nnnn</div> : <Link to="/singUp">
+                                        <h6 className='font-h6 mt-4'>Become a Vendor</h6>
+                                    </Link>
+                                }
+
+
                                 {/* <a href="#"><h6 className='font-h6'>Affiliate Program</h6></a>
                                 <a href="#"><h6 className='font-h6'>Farm Business</h6></a>
                                 <a href="#"><h6 className='font-h6'>Farm Careers</h6></a> */}
                                 <Link to="/seller">
-                                <h6 className='font-h6'>Our Suppliers</h6>
+                                    <h6 className='font-h6'>Our Suppliers</h6>
                                 </Link>
                                 {/* <a href="#"><h6 className='font-h6'>Accessibility</h6></a> */}
                                 <Link to="/promotions">
                                     <h6 className='font-h6'>Promotions</h6>
-                                    </Link>
+                                </Link>
 
 
                             </div>
@@ -120,9 +132,9 @@ const Footer = () => {
                         <div className="col-lg-2 col-md-3">
                             <div className="p-3 ">
                                 <h1 className='font-h1'>Popular</h1>
-                               <Link to="">
-                               <h6 className='font-h6'>Milk & Flavoured Milk</h6>
-                               </Link>
+                                <Link to="/shop">
+                                    <h6 className='font-h6'>Milk & Flavoured Milk</h6>
+                                </Link>
                                 {/* <h6 className='font-h6'>Butter and Margarine</h6>
                                 <h6 className='font-h6'>Eggs Substitutes</h6>
                                 <h6 className='font-h6'>Marmalades</h6>
@@ -139,20 +151,20 @@ const Footer = () => {
                                 <div className='my-4'>
                                     <Link to="">
                                         <img src={logo2} alt="" className="w-50 p-1" />
-                                        </Link>
-
-                                    <Link to= "">
-                                    <img src={logo3} alt="" className="w-50 p-1" />
                                     </Link>
-                                        
-                                    
+
+                                    <Link to="">
+                                        <img src={logo3} alt="" className="w-50 p-1" />
+                                    </Link>
+
+
                                 </div>
                                 <div>
-                                <Link to= "">
-                                <img src={logo4} alt="" className="img-fluid my-3" />
-                                </Link>
-                                        
-                                    
+                                    <Link to="">
+                                        <img src={logo4} alt="" className="img-fluid my-3" />
+                                    </Link>
+
+
                                 </div>
                             </div>
                         </div>
