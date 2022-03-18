0a974f3f74c6a9acaafe8865c1e647710fa82ce3
nafish nobel <nobel15-11651@diu.edu.bd>
2022-03-18 22:49:15 +0600

    support section clear

--- src/Components/SupportCenter/SupportCenter.tsx
+++ src/Components/SupportCenter/SupportCenter.tsx
@@ -33,11 +33,11 @@ const SupportCenter = () => {
         <div className=''>
 
             <h1 className='text-center fw-bold text-white m-1 bg-success p-1 rounded'>Welcome To Our <span className='text-danger'>Support</span> Section</h1>
-
+                <h3 className='text-center fw-bold text-warning'>We are here for your service please choose your option down below</h3>
             <div className="container">
                 <div className="row g-2">
-                    <div className="col-6">
-                        <div className="p-3 border bg-light">
+                    <div className="col-lg-6">
+                        <div className="p-3 ">
 
                             <Row xs={1} md={2} className="g-4">
                                 {
@@ -45,21 +45,21 @@ const SupportCenter = () => {
                                     SupportData.map((sData) =>
 
                                         <Col>
-                                            <Card className='my-1'>
-                                                <Card.Img variant="top" className='img-fluid'   src={sData.images} />
+                                            <Card className='my-1 shadow'>
+                                                <Card.Img variant="top" className='img-fluid' src={sData.images} />
                                                 <Card.Body>
                                                     <Card.Title className='fw-bold text-center text-warning bg-light rounded'>{sData.name}</Card.Title>
                                                     <Card.Text className=''>
-                                                       <span >
-                                                       {sData.details}
-                                                       </span> 
+                                                        <span >
+                                                            {sData.details}
+                                                        </span>
                                                     </Card.Text>
 
-                                                        <a className='my-1 text-center' href="http://nurnafishahmednobel@gmail.com">
+                                                    <a className='my-1 text-center' href="http://nurnafishahmednobel@gmail.com">
                                                         <button type="button" className="btn btn-info">Email</button>
-                                                        </a>
-                                                    
-                                                    
+                                                    </a>
+
+
                                                 </Card.Body>
                                             </Card>
                                         </Col>
@@ -72,9 +72,12 @@ const SupportCenter = () => {
                             </Row>
                         </div>
                     </div>
-                    <div className="col-6">
-                        <div className="p-3 border bg-light">
-                            <h1>second part</h1>
+                    <div className="col-lg-6">
+                        <div className="p-3 ">
+
+                            <img className='img-fluid rounded' src="https://cdn.dribbble.com/users/2054054/screenshots/6257329/interview.gif" alt=""/>
+
+
                         </div>
                     </div>
 
